5000b7f76c14bd4f012b492cff347b9d1b60169b
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-06-18 11:26:13 +0700

    Remove fontawesome, add iconify for better icons

--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -2,10 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-
-library.add(fas)
 
 const app = createApp(App)
 
