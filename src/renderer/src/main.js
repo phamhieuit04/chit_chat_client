1e06dc4f3c2ec65cee0abbbb82d4996f6db2a41e
HieuPNT <tomnguyenhieu2004@gmail.com>
2025-06-18 23:22:36 +0700

    Add pinia ✨

--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -1,8 +1,14 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
 const app = createApp(App)
+app.use(pinia)
 
 app.mount('#app')
